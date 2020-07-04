e69def359e816ced588187e38a4b5fe45a9f5612
Hank <hassanien.kaissi@gmail.com>
2020-07-04 12:39:17 +0400

    removed quotation mark

--- AutoRepo/project.bat
+++ AutoRepo/project.bat
@@ -1,4 +1,4 @@
 @echo off
-if [%1]==[dir] (start "C:\Users\HASSANIN\Desktop\PythonProj") && exit
+if [%1]==[dir] (start C:\Users\HASSANIN\Desktop\PythonProj) && exit
 if not [%2]==[.] (cd "C:\Users\HASSANIN\Desktop\PythonProj")
 "%~dp0\gitrepo.py" %1
