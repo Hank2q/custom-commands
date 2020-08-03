327012c1f6fd575256bf2afb77f74fa1ad6639d3
Hank <hassanien.kaissi@gmail.com>
2020-08-04 03:16:07 +0400

    added making web page funcionality to make.bat

--- make.bat
+++ make.bat
@@ -1,2 +1,7 @@
 @echo off
-@echo File: [%1] created 2> %1
+if %1==webpage (CALL :Make index.html & CALL :Make style.css & CALL :Make app.js & EXIT /B 0) else (CALL :Make %1 & EXIT /B 0)
+
+:Make
+echo File: [%~1] created 
+echo.> %~1
+
