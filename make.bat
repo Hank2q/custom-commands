84f74c82c7c97112104cb282f96f766b1e0816fe
Hank <hassanien.kaissi@gmail.com>
2020-10-03 04:54:09 +0400

    added ls command

--- make.bat
+++ make.bat
@@ -2,6 +2,5 @@
 if %1==webpage (CALL :Make index.html & CALL :Make style.css & CALL :Make app.js & EXIT /B 0) else (CALL :Make %1 & EXIT /B 0)
 
 :Make
-echo File: [%~1] created 
-echo.> %~1
+echo File: [%~1] created 2> %~1
 
